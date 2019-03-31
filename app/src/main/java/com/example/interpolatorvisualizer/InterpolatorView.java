c2bf0a11680391de4676b7894fc861a9812be577
Alan Evans <thealanevans@gmail.com>
2019-03-31 12:00:16 -0300

    Show alpha, hide steep lines.

--- app/src/main/java/com/example/interpolatorvisualizer/InterpolatorView.java
+++ app/src/main/java/com/example/interpolatorvisualizer/InterpolatorView.java
@@ -15,169 +15,173 @@ import android.view.animation.LinearInterpolator;
 
 public final class InterpolatorView extends View {
 
-	protected static final String TAG = "InterpolatorView";
-	private final Paint paint = new Paint();
-	private final Converter converter = new Converter();
-	private Interpolator interpolator = new LinearInterpolator();
-	private ValueAnimator animator;
-	protected float position = -1;
+    protected static final String TAG = "InterpolatorView";
+    private final Paint paint = new Paint();
+    private final Converter converter = new Converter();
+    private Interpolator interpolator = new LinearInterpolator();
+    private ValueAnimator animator;
+    protected float position = -1;
 
-	public InterpolatorView(Context context) {
-		super(context);
-	}
+    public InterpolatorView(Context context) {
+        super(context);
+    }
 
-	public InterpolatorView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public InterpolatorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public InterpolatorView(Context context, AttributeSet attrs,
-			int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-	}
+    public InterpolatorView(Context context, AttributeSet attrs,
+                            int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
-		paint.setTextSize(40);
+        paint.setTextSize(40);
 
-		int width = getWidth();
-		int height = getHeight();
-		converter.setWidth(width);
-		converter.setHeight(height);
+        int width = getWidth();
+        int height = getHeight();
+        converter.setWidth(width);
+        converter.setHeight(height);
 
-		canvas.drawColor(Color.BLACK);
+        canvas.drawColor(Color.BLACK);
 
-		drawline(canvas, Color.LTGRAY, -2, 0, 2, 0);
-		int boundColor = Color.GRAY;
-		drawline(canvas, boundColor, 0, -2, 0, 2);
-		drawline(canvas, boundColor, 1, -2, 1, 2);
-		drawline(canvas, boundColor, -2, 1, 2, 1);
+        drawline(canvas, Color.LTGRAY, -2, 0, 2, 0);
+        int boundColor = Color.GRAY;
+        drawline(canvas, boundColor, 0, -2, 0, 2);
+        drawline(canvas, boundColor, 1, -2, 1, 2);
+        drawline(canvas, boundColor, -2, 1, 2, 1);
 
-		drawText(canvas, Color.WHITE, "0", 0, 0, 0, paint.getTextSize());
-		drawText(canvas, Color.WHITE, "1", 1, 0, 0, paint.getTextSize());
-		drawText(canvas, Color.WHITE, "1", 0, 1);
+        drawText(canvas, Color.WHITE, "0", 0, 0, 0, paint.getTextSize());
+        drawText(canvas, Color.WHITE, "1", 1, 0, 0, paint.getTextSize());
+        drawText(canvas, Color.WHITE, "1", 0, 1);
 
-		for (int x = 0; x < width - 1; x++) {
-			float x1 = converter.fromScreen(x);
-			float x2 = converter.fromScreen(x + 1);
-			float y1 = interpolator.getInterpolation(x1);
-			float y2 = interpolator.getInterpolation(x2);
+        for (int x = 0; x < width - 1; x++) {
+            float x1 = converter.fromScreen(x);
+            float x2 = converter.fromScreen(x + 1);
+            float y1 = interpolator.getInterpolation(x1);
+            float y2 = interpolator.getInterpolation(x2);
 
-			int color = Color.RED;
-			if ((x1 >= 0 || x2 >= 0) && (x1 <= 1 || x2 <= 1))
-				color = Color.GREEN;
-			drawlineScreenX(canvas, color, x, y1, x + 1, y2);
-		}
+            int color = Color.RED;
+            if ((x1 >= 0 || x2 >= 0) && (x1 <= 1 || x2 <= 1))
+                color = Color.GREEN;
+            paint.setColor(color);
 
-		paint.setColor(Color.WHITE);
-		canvas.drawText(interpolator.getClass().getSimpleName(), 0,
-				paint.getTextSize(), paint);
+            final float screenY1 = converter.toScreenY(y1);
+            final float screenY2 = converter.toScreenY(y2);
 
-		if (position >= 0) {
-			float x1 = position;
-			float y1 = interpolator.getInterpolation(x1);
-			canvas.drawCircle(converter.toScreenX(x1), converter.toScreenY(y1),
-					10, paint);
-			canvas.drawCircle(converter.toScreenX(0), converter.toScreenY(y1),
-					10, paint);
-			canvas.drawCircle(converter.toScreenX(x1), converter.toScreenY(0),
-					10, paint);
-		}
-	}
+            if (Math.abs(screenY1 - screenY2) < 100) {
+                canvas.drawLine(x, screenY1, x + 1,
+                        screenY2, paint);
+            }
+        }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-			play();
-			return true;
-		}
-		return true;
-	}
+        paint.setColor(Color.WHITE);
+        canvas.drawText(interpolator.getClass().getSimpleName(), 0,
+                paint.getTextSize(), paint);
 
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		int measuredWidth = getMeasuredWidth();
-		setMeasuredDimension(measuredWidth, measuredWidth);
-	}
+        if (position >= 0) {
+            float x1 = position;
+            float y1 = interpolator.getInterpolation(x1);
+            canvas.drawCircle(converter.toScreenX(x1), converter.toScreenY(y1),
+                    10, paint);
+            canvas.drawCircle(converter.toScreenX(0), converter.toScreenY(y1),
+                    10, paint);
+            canvas.drawCircle(converter.toScreenX(x1), converter.toScreenY(0),
+                    10, paint);
+            paint.setAlpha(Math.max(0, Math.min(255, (int) (y1 * 255))));
+            canvas.drawCircle(converter.toScreenX(-0.5f), converter.toScreenY(0.5f),
+                    20, paint);
+        }
+    }
 
-	private void drawText(Canvas canvas, int color, String string, float x,
-			float y) {
-		paint.setColor(color);
-		canvas.drawText(string, converter.toScreenX(x), converter.toScreenY(y),
-				paint);
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+            play();
+            return true;
+        }
+        return true;
+    }
 
-	private void drawText(Canvas canvas, int color, String string, float x,
-			float y, float screenDeltaX, float screenDeltaY) {
-		paint.setColor(color);
-		canvas.drawText(string, converter.toScreenX(x) + screenDeltaX,
-				converter.toScreenY(y) + screenDeltaY, paint);
-	}
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        setMeasuredDimension(measuredWidth, measuredWidth);
+    }
 
-	private void drawline(Canvas canvas, int color, int fromX, int fromY,
-			int toX, int toY) {
-		paint.setColor(color);
-		canvas.drawLine(converter.toScreenX(fromX), converter.toScreenY(fromY),
-				converter.toScreenX(toX), converter.toScreenY(toY), paint);
-	}
+    private void drawText(Canvas canvas, int color, String string, float x,
+                          float y) {
+        paint.setColor(color);
+        canvas.drawText(string, converter.toScreenX(x), converter.toScreenY(y),
+                paint);
+    }
 
-	private void drawlineScreenX(Canvas canvas, int color, int fromX,
-			float fromY, int toX, float toY) {
-		paint.setColor(color);
-		canvas.drawLine(fromX, converter.toScreenY(fromY), toX,
-				converter.toScreenY(toY), paint);
-	}
+    private void drawText(Canvas canvas, int color, String string, float x,
+                          float y, float screenDeltaX, float screenDeltaY) {
+        paint.setColor(color);
+        canvas.drawText(string, converter.toScreenX(x) + screenDeltaX,
+                converter.toScreenY(y) + screenDeltaY, paint);
+    }
 
-	public void setInterpolator(Interpolator interpolator) {
-		this.interpolator = interpolator;
-		invalidate();
-	}
+    private void drawline(Canvas canvas, int color, int fromX, int fromY,
+                          int toX, int toY) {
+        paint.setColor(color);
+        canvas.drawLine(converter.toScreenX(fromX), converter.toScreenY(fromY),
+                converter.toScreenX(toX), converter.toScreenY(toY), paint);
+    }
 
-	private static class Converter {
+    public void setInterpolator(Interpolator interpolator) {
+        this.interpolator = interpolator;
+        invalidate();
+    }
 
-		private float scaleX;
-		private float scaleY;
+    private static class Converter {
 
-		public float toScreenX(float x) {
-			return (x + 2) * scaleX;
-		}
+        private float scaleX;
+        private float scaleY;
 
-		public float fromScreen(float x) {
-			return x / scaleX - 2f;
-		}
+        public float toScreenX(float x) {
+            return (x + 2) * scaleX;
+        }
 
-		public float toScreenY(float y) {
-			return (4 - y - 2) * scaleY;
-		}
+        public float fromScreen(float x) {
+            return x / scaleX - 2f;
+        }
 
-		public void setWidth(float width) {
-			scaleX = width / 4f;
-		}
+        public float toScreenY(float y) {
+            return (4 - y - 2) * scaleY;
+        }
 
-		public void setHeight(float height) {
-			scaleY = height / 4f;
-		}
+        public void setWidth(float width) {
+            scaleX = width / 4f;
+        }
 
-	}
+        public void setHeight(float height) {
+            scaleY = height / 4f;
+        }
 
-	public void play() {
-		if (animator != null)
-			animator.cancel();
-		animator = ValueAnimator.ofFloat(0, 1);
+    }
+
+    public void play() {
+        if (animator != null)
+            animator.cancel();
+        animator = ValueAnimator.ofFloat(0, 1);
 		animator.setDuration(5000);
-		animator.setInterpolator(new LinearInterpolator());
-		animator.addUpdateListener(new AnimatorUpdateListener() {
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new AnimatorUpdateListener() {
 
-			@Override
-			public void onAnimationUpdate(ValueAnimator animator) {
-				position = ((Float) animator.getAnimatedValue()).floatValue();
-				Log.d(TAG, String.format("Pos: %.2f", position));
-				invalidate();
-			}
-		});
-		animator.start();
-	}
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                position = ((Float) animator.getAnimatedValue()).floatValue();
+                Log.d(TAG, String.format("Pos: %.2f", position));
+                invalidate();
+            }
+        });
+        animator.start();
+    }
 
 }
